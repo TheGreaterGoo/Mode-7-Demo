6588ce583e1109dff41d19a5b9f5fd9044edc788
TheGreaterGoo <thegreatergoo@gmail.com>
2023-07-10 14:36:40 -0700

    basic floorcasting

--- program.py
+++ program.py
@@ -1,7 +1,5 @@
-import src.projector
-
 try:
-    with open("projector.py") as f:
+    with open("src/projector.py") as f:
         exec(f.read())
 except Exception as N:
     print("Terminated - " + str(N))
--- src/projector.py
+++ src/projector.py
@@ -1,35 +1,68 @@
 import pygame
 import numpy
-pygame.init()
 
-w = pygame.display.set_mode([400, 400])
+def game():
+    pygame.init()
 
-# define floor specifications
-hres = 120
-half_vres = 100
+    w = pygame.display.set_mode([800, 600])
+    clock = pygame.time.Clock()
 
-mod = hres / 60 # scaling factor (60° FOV)
-posx, posy, rot = 0, 0, 0 # player
+    # define floor specifications
+    hres = 120
+    half_vres = 100
 
-frame = numpy.random.uniform(0, 1, (80, 60, 3))
+    mod = hres / 60 # scaling factor (60 degree FOV)
+    posx, posy, rot = 0, 0, 0 # player
 
-drawing = True
-while drawing:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            drawing = False
+    frame = numpy.random.uniform(0, 1, (hres, half_vres * 2, 3))
 
-    for i in range(hres):
-        rot_i = rot + numpy.deg2rad(i/mod - 30) # fov represented by rot +/- 30°
-        sin, cos = numpy.sin(rot_i), numpy.cos(rot_i)
+    drawing = True
+    while drawing:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                drawing = False
 
-        for j in range(half_vres):
-            n = half_vres / (half_vres - j)
-            x, y = posx + cos * n, posy + sin * n
+        for i in range(hres): # iterating through the columns in the screen
+            # rot_i is the direction of each column part of the fov in the frame
+            rot_i = rot + numpy.deg2rad(i/mod - 30) # fov represented by rot +/- 30 degrees
+            sin, cos = numpy.sin(rot_i), numpy.cos(rot_i)
 
-    surface = pygame.surfarray.make_surface(frame * 255)
-    surface = pygame.transform.scale(surface, (400, 400))
+            # iterating through the bottom half rows in the screen (floor doesn't go past halfway point)
+            # distance from player to screen is inversely proportional to the perception of floor distance
+            # basically the higher the player looks on the screen, the further away the floor is projected
+            for j in range(half_vres):
+                n = half_vres / (half_vres - j)
+                x, y = posx + cos * n, posy + sin * n
 
-    w.blit(surface, (0, 0))
+                if int(x) % 2 == int(y) % 2:
+                    frame[i][half_vres * 2 - j - 1] = [0, 0, 0]
+                else:
+                    frame[i][half_vres * 2 - j - 1] = [1, 1, 1]
 
-    pygame.display.flip()
+        surface = pygame.surfarray.make_surface(frame * 255)
+        surface = pygame.transform.scale(surface, (800, 600))
+
+        w.blit(surface, (0, 0))
+
+        pygame.display.update()
+        posx, posy, rot = movement(posx, posy, rot, pygame.key.get_pressed())
+
+def movement(posx, posy, rot, keys):
+    if keys[pygame.K_LEFT]:
+        rot -= 0.1
+
+    if keys[pygame.K_RIGHT]:
+        rot += 0.1
+
+    if keys[pygame.K_UP]:
+        posx += numpy.cos(rot) * 0.1
+        posy += numpy.sin(rot) * 0.1
+
+    if keys[pygame.K_DOWN]:
+        posx -= numpy.cos(rot) * 0.1
+        posy -= numpy.sin(rot) * 0.1
+
+    return posx, posy, rot
+
+if __name__ == "__main__":
+    game()
