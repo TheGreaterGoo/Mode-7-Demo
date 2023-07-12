e6c357eeddd7cf5b28bd117bbd5ba74e75ff3e68
TheGreaterGoo <thegreatergoo@gmail.com>
2023-07-12 08:52:28 -0700

    New textures

--- src/projector.py
+++ src/projector.py
@@ -1,5 +1,7 @@
 import pygame
 import numpy
+from numba import njit
+import asyncio
 
 def game():
     pygame.init()
@@ -13,56 +15,94 @@ def game():
 
     mod = hres / 60 # scaling factor (60 degree FOV)
     posx, posy, rot = 0, 0, 0 # player
+    posz, vz, az = 1, 0.01, 0.001
 
     frame = numpy.random.uniform(0, 1, (hres, half_vres * 2, 3))
+    bg = pygame.image.load("assets/mountains.png")
+    bg = pygame.surfarray.array3d(pygame.transform.scale(bg, (360, half_vres * 2)))
+
+    floor_assets = ["assets/grass.png", "assets/rocks.png", "assets/dirt.png"]
+    floor = pygame.surfarray.array3d(pygame.image.load(floor_assets[2]))
 
     drawing = True
     while drawing:
+
+        pygame.display.set_caption(f"FPS: {clock.get_fps():.2f}")
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 drawing = False
 
-        for i in range(hres): # iterating through the columns in the screen
-            # rot_i is the direction of each column part of the fov in the frame
-            rot_i = rot + numpy.deg2rad(i/mod - 30) # fov represented by rot +/- 30 degrees
-            sin, cos = numpy.sin(rot_i), numpy.cos(rot_i)
+        frame = new_frame(posx, posy, rot, frame, bg, floor, hres, half_vres, mod, posz)
 
-            # iterating through the bottom half rows in the screen (floor doesn't go past halfway point)
-            # distance from player to screen is inversely proportional to the perception of floor distance
-            # basically the higher the player looks on the screen, the further away the floor is projected
-            for j in range(half_vres):
-                n = half_vres / (half_vres - j)
-                x, y = posx + cos * n, posy + sin * n
-
-                if int(x) % 2 == int(y) % 2:
-                    frame[i][half_vres * 2 - j - 1] = [0, 0, 0]
-                else:
-                    frame[i][half_vres * 2 - j - 1] = [1, 1, 1]
 
         surface = pygame.surfarray.make_surface(frame * 255)
         surface = pygame.transform.scale(surface, (800, 600))
-
         w.blit(surface, (0, 0))
 
         pygame.display.update()
-        posx, posy, rot = movement(posx, posy, rot, pygame.key.get_pressed())
+        posx, posy, rot, posz = movement(posx, posy, rot, pygame.key.get_pressed(), clock.tick(), posz, vz, az)
 
-def movement(posx, posy, rot, keys):
+def movement(posx, posy, rot, keys, et, posz, vz, az):
     if keys[pygame.K_LEFT]:
-        rot -= 0.1
+        rot -= 0.002 * et
 
     if keys[pygame.K_RIGHT]:
-        rot += 0.1
+        rot += 0.002 * et
 
     if keys[pygame.K_UP]:
-        posx += numpy.cos(rot) * 0.1
-        posy += numpy.sin(rot) * 0.1
+        posx += numpy.cos(rot) * 0.002 * et
+        posy += numpy.sin(rot) * 0.002 * et
 
     if keys[pygame.K_DOWN]:
-        posx -= numpy.cos(rot) * 0.1
-        posy -= numpy.sin(rot) * 0.1
+        posx -= numpy.cos(rot) * 0.002 * et
+        posy -= numpy.sin(rot) * 0.002 * et
 
-    return posx, posy, rot
+    if keys[pygame.K_SPACE]:
+        posz, vz, az = jump(posz, vz, az)
+
+    return posx, posy, rot, posz
+
+def jump(posz, vz, az):
+    if posz > 0.5:
+        posz -= vz
+        vz -= az
+    else:
+        vz *= -1
+        az *= -1
+    return posz, vz, az
+
+def is_touching_ground():
+    return False
+
+@njit()
+def new_frame(posx, posy, rot, frame, bg, floor, hres, half_vres, mod, z):
+    for i in range(hres):  # iterating through the columns in the screen
+        # rot_i is the direction of each column part of the fov in the frame
+        rot_i = rot + numpy.deg2rad(i / mod - 30)  # fov represented by rot +/- 30 degrees
+        sin, cos, cos2 = numpy.sin(rot_i), numpy.cos(rot_i), numpy.cos(numpy.deg2rad(i / mod - 30))
+
+        # Extracting and setting an entire column of the background to the frame
+        frame[i][:] = bg[int(numpy.rad2deg(rot_i) % 359)][:] / 255
+
+        # iterating through the bottom half rows in the screen (floor doesn't go past halfway point)
+        # distance from player to screen is inversely proportional to the perception of floor distance
+        # basically the higher the player looks on the screen, the further away the floor is projected
+        for j in range(half_vres):
+            n = (half_vres / (half_vres - j)) / (cos2 * z)
+            x, y = posx + cos * n, posy + sin * n
+
+            # Drawing the floor
+            shade = 0.25 + 0.8 * (1 - j / half_vres)
+
+            floor_x, floor_y = int(x * 2 % 1 * 100), int(y * 2 % 1 * 100)
+            frame[i][half_vres * 2 - j - 1] = shade * floor[floor_x][floor_y] / 255
+
+    return frame
+            # if int(x) % 2 == int(y) % 2:
+            #     frame[i][half_vres * 2 - j - 1] = [0, 0, 0]
+            # else:
+            #     frame[i][half_vres * 2 - j - 1] = [1, 1, 1]
 
 if __name__ == "__main__":
     game()
